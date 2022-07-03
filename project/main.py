d1d2ec2ef9ed9bd5f0edd36e6f68ecb5957690a0
Cris <68723063+CrxsTagz@users.noreply.github.com>
2022-07-02 21:58:01 -0400

    added a json file to execute a function. The function was added to main.py, added a JSON button (change name)

--- project/main.py
+++ project/main.py
@@ -2,6 +2,8 @@
 
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
+from random import random
+from fpdf import FPDF
 
 main = Blueprint('main', __name__)
 
@@ -19,3 +21,28 @@ def profile():
 @main.route('/menu')
 def menu():
     return render_template('menu.html')
+
+@main.route('/json')
+def json():
+    return render_template('json.html')
+
+@main.route('/getpdf')
+def getpdf():
+    wpascore = random()
+    fwfhs = random()
+    #save FPDF class into a variable pdf
+    pdf = FPDF()
+
+#Add page
+    pdf.add_page()
+
+    pdf.set_font("Arial", size= 13)
+
+#create cells 
+
+    pdf.cell(200, 10,  txt= f"Tu score en wpa es: {wpascore}", ln= 1, align= 'L')
+    pdf.cell(200, 10,  txt= f"Tu score en wpa es: {fwfhs}", ln= 2, align= 'L')
+
+#save the pdf with the .pdf extension
+
+    pdf.output("Prueba.pdf")
